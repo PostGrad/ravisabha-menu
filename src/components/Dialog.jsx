361f2cfd879d982c100369ceb4393f1c429f5bf7
Pranay <pranaypatel008@gmail.com>
2023-07-13 11:47:48 +0530

    new data for ingrediants

--- src/components/Dialog.jsx
+++ src/components/Dialog.jsx
@@ -8,7 +8,7 @@ function Dialog(props) {
   }
   return (
     <div className="fixed inset-0 z-50 overflow-auto bg-smoke-light flex">
-      <div className="relative p-8 bg-white w-full max-w-md m-auto flex-col flex rounded-lg">
+      <div className="relative p-8  bg-orange-200 w-full max-w-md m-auto flex-col flex rounded-lg">
         <div>{props.children}</div>
         <span className="absolute top-0 right-0 p-4">
           <IconButton onClick={() => onClose()}>
