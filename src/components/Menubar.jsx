45f3ede0be3418cbb9864ee1f80de9951262f2e6
PostGrad <pranaypatel008@gmail.com>
2023-08-01 22:02:59 +0530

    make menubar responsive

--- src/components/Menubar.jsx
+++ src/components/Menubar.jsx
@@ -1,48 +1,80 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import EventList from "./EventList";
 
 function Menubar() {
   return (
-    <div className="navbar bg-white py-4 shadow-md w-screen">
-      <div className="flex-1">
-        <Link to="/" className="btn btn-ghost normal-case text-xl">
-          BUPA Kitchen King
+    <div className="navbar bg-base-100 w-screen">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <Link to="/">Event List</Link>
+            </li>
+            {/* <li>
+              <a>Parent</a>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </li> */}
+            <li>
+              <Link to="/evententryform">New Event</Link>
+            </li>
+          </ul>
+        </div>
+        <Link className="btn btn-ghost normal-case text-xl" to="/">
+          BUPA Kitchen
         </Link>
       </div>
-      <div className="flex-none">
+      <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
           <li>
-            <Link to="/">List of Events</Link>
-          </li>{" "}
-          <li>
-            <Link to="/evententryform">Create New Event</Link>
+            <Link to="/">Event List</Link>
           </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2 bg-base-100">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
+          {/* <li tabIndex={0}>
+            <details>
+              <summary>Parent</summary>
+              <ul className="p-2">
+                <li>
+                  <a>Submenu 1</a>
+                </li>
+                <li>
+                  <a>Submenu 2</a>
+                </li>
+              </ul>
+            </details>
+          </li> */}
           <li>
-            <a>Log Out</a>
+            <Link to="/evententryform">New Event</Link>
           </li>
         </ul>
+      </div>
+      <div className="navbar-end">
+        <a className="btn btn-error">Log Out</a>
       </div>
     </div>
   );
--- src/data/userData.js
+++ src/data/userData.js
@@ -4,6 +4,21 @@ export const Users = [
     userName: "Pranay Patel",
     role: ["super"],
   },
+  {
+    email: "priyank@vithani.com",
+    userName: "Priyank Vithani",
+    role: ["super"],
+  },
+  {
+    email: "shailesh@kevadiya.com",
+    userName: "Shailesh Kevadiya",
+    role: ["admin"],
+  },
+  {
+    email: "yogesh@setruja.com",
+    userName: "Yogesh Detruja",
+    role: ["admin"],
+  },
 ];
 
 export const Roles = ["admin", "normal", "super"];
